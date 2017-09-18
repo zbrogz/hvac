36b5a90ac86406635cec5d4e34be5f5d562aa9f6
Zach Brogan <zachbrogan@gmail.com>
2017-09-18 15:52:56 -0600

    Fixes
    Refactored code into smaller functions.
    Added error check to JSON parsing

--- hvac.ino
+++ hvac.ino
@@ -17,6 +17,16 @@ void setup() {
   pinMode(D8, OUTPUT);
   Serial.begin(115200);
   Serial.println();
+  connectToWifiAP();
+}
+
+void loop() {
+  WiFiClientSecure client;
+  sendRequestToAPI(&client);
+  getResponseFromAPI(&client);
+}
+
+void connectToWifiAP() {
   Serial.print("connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -30,12 +40,11 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
-void loop() {
-    // Use WiFiClientSecure class to create TLS connection
-  WiFiClientSecure client;
+void connectToAPI(WiFiClientSecure* client) {
+  // Use WiFiClientSecure class to create TLS connection
   Serial.print("connecting to ");
   Serial.println(host);
-  if (!client.connect(host, httpsPort)) {
+  if (!client->connect(host, httpsPort)) {
     Serial.println("connection failed");
     return;
   }
@@ -45,39 +54,39 @@ void loop() {
   } else {
     Serial.println("certificate doesn't match");
   }*/
+  
+}
 
+void sendRequestToAPI(WiFiClientSecure* client) {
   String url = "/testStage/hvac/testHvacID";
   Serial.print("requesting URL: ");
   Serial.println(url);
 
-  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+  client->print(String("GET ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" +
                "User-Agent: BuildFailureDetectorESP8266\r\n" +
                "Connection: close\r\n\r\n");
-
+  
   Serial.println("request sent");
-  //String line = client.readString();//client.readStringUntil('\n');
-  //Serial.println(line);
   delay(500);
+}
+
+void getResponseFromAPI(WiFiClientSecure* client) {
   String json = "";
-  String line = "";
-  boolean httpBody = false;
-  while (client.available()) {
-    line = client.readStringUntil('\r');
-    /*if (!httpBody && line.charAt(1) == '{') {
-      httpBody = true;
-    }
-    if (httpBody) {
-      json += line;
-    }*/
+  // Read https response until json data
+  while (client->available()) {
+    json = client->readStringUntil('\r');
   }
   StaticJsonBuffer<400> jsonBuffer;
   Serial.println("Got data:");
-  Serial.println(line);
-  JsonObject& root = jsonBuffer.parseObject(line);
+  Serial.println(json);
+  JsonObject& root = jsonBuffer.parseObject(json);
+  if(!root.success()) {
+    Serial.println("JSON PARSING FAILED");
+    return;
+  }
   Serial.println("JSON PARSED");
   const char* heater = root["heater"];
-  Serial.println(heater);
   
   if(!strcmp("on", heater)) {
     Serial.println("Heater is on");
@@ -88,3 +97,4 @@ void loop() {
     digitalWrite(D8, LOW); // LED OFF
   }
 }
+
